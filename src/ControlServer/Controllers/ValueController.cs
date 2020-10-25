b6d5a963068727032d78cf5b67354cd3115de3c3
Ynnoj <pmb72@gmx.de>
2020-10-25 15:54:18 +0100

    Add Put Action

--- src/ControlServer/Controllers/ValueController.cs
+++ src/ControlServer/Controllers/ValueController.cs
@@ -49,7 +49,7 @@ namespace ControlServer.Controllers
             XmlElement indexOffset = document.CreateElement("indexOffset");
             indexOffset.AppendChild(document.CreateTextNode(offset));
 
-            XmlElement cbLen = document.CreateElement("indexOffset");
+            XmlElement cbLen = document.CreateElement("cbLen");
             cbLen.AppendChild(document.CreateTextNode("1"));
 
             document.AppendChild(envelope);
@@ -89,14 +89,65 @@ namespace ControlServer.Controllers
                 .Element("ppData")
                 .Value;
 
-            byte[] buffer = System.Convert.FromBase64String(value);
+            byte[] buffer = Convert.FromBase64String(value);
             return buffer[0];
         }
 
         [HttpPut]
-        public async Task<ActionResult> Put(CancellationToken cancel, string id, string group, string offset)
+        public async Task<ActionResult> Put(CancellationToken cancel, string group, string offset, [FromBody]bool value)
         {
-            // await _client.WriteValueAsync<string>(id, value, cancel);
+                  XmlDocument document = new XmlDocument();
+
+            XmlElement envelope = document.CreateElement("soap:Envelope", "http://schemas.xmlsoap.org/soap/envelope/");
+            envelope.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            envelope.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+
+            XmlElement body = document.CreateElement("soap:Body", "http://schemas.xmlsoap.org/soap/envelope/");
+            AddAttribute(body, "soap:encodingStyle", "http://schemas.xmlsoap.org/soap/envelope/", "http://schemas.xmlsoap.org/soap/encoding/");
+
+            XmlElement write = document.CreateElement("q1:Write", "http://beckhoff.org/message/");
+            XmlElement netId = document.CreateElement("netId");
+            netId.AppendChild(document.CreateTextNode("192.168.168.11.1.1"));
+
+            XmlElement nPort = document.CreateElement("nPort");
+            nPort.AppendChild(document.CreateTextNode("801"));
+
+            XmlElement indexGroup = document.CreateElement("indexGroup");
+            indexGroup.AppendChild(document.CreateTextNode(group));
+
+            XmlElement indexOffset = document.CreateElement("indexOffset");
+            indexOffset.AppendChild(document.CreateTextNode(offset));
+
+            XmlElement pData = document.CreateElement("pData");
+            pData.AppendChild(document.CreateTextNode(Convert.ToBase64String(new byte[] { Convert.ToByte(value) })));
+
+            document.AppendChild(envelope);
+            envelope.AppendChild(body);
+            body.AppendChild(write);
+            write.AppendChild(netId);
+            write.AppendChild(nPort);
+            write.AppendChild(indexGroup);
+            write.AppendChild(indexOffset);
+            write.AppendChild(pData);
+
+            StringBuilder builder = new StringBuilder();
+            using (StringWriter writer = new StringWriter(builder))
+            {
+                document.Save(writer);
+            }
+
+            string content = builder.ToString();
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage();
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri("http://192.168.168.11/TcAdsWebService/TcAdsWebService.dll");
+                request.Content = new StringContent(content);
+                request.Headers.Add("SOAPAction", "http://beckhoff.org/action/TcAdsSync.Read");
+                
+                HttpResponseMessage response = await client.SendAsync(request);
+                content = await response.Content.ReadAsStringAsync();
+            }
             return this.Ok();
         }
 
